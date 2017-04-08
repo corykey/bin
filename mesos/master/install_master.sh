6ef021adea6d5cc606ade9964b6348e9b38e1034
corykey <dev@corykey.com>
2017-04-08 22:34:17 +0200

    add marathon and java 8 runtime - Mesos Vagrantfile

--- mesos/master/install_master.sh
+++ mesos/master/install_master.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND="noninteractive"
+
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
-echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | tee /etc/apt/sources.list.d/mesosphere.list
 
-apt-add-repository ppa:webupd8team/java \
-	&& apt-get -y --force-yes install oracle-java8-installer
+echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | tee /etc/apt/sources.list.d/mesosphere.list
 
-apt-get -y update \
-	&& apt-get -y --force-yes install mesosphere \
-	&& apt-get -y --force-yes install mesos
+apt-get -y update && apt-get upgrade -y \
+    && apt-get install -y --force-yes oracle-java8-installer \
+    && apt-get install -y --force-yes mesos marathon chronos
